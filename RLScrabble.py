17d2129f13e50a55c15b971eac30371557bc9929
Unknown <matiss.apinis@gmail.com>
2019-04-28 22:56:34 +0300

    Implemented Mattel-style game board

--- RLScrabble.py
+++ RLScrabble.py
@@ -10,29 +10,34 @@ Date created:   2019/04/28
 Date edited:    2019/04/28
 '''
 
+# Libraries:
 import pygame
+
+# Local files:
+import ScrabbleBoard
+
 pygame.init()
 
-SIZE_DISPLAY_WIDTH = 800
-SIZE_DISPLAY_HEIGHT = 600
-DISPLAY_SCRABBLE = pygame.display
-DISPLAY_SCRABBLE.set_mode((SIZE_DISPLAY_WIDTH,SIZE_DISPLAY_HEIGHT))
-DISPLAY_SCRABBLE.set_caption("RLScrabble")
+SIZE_DISPLAY_WIDTH = 1200
+SIZE_DISPLAY_HEIGHT = 900
 
-COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_RED = (255, 0, 0)
-COLOR_GREEN = (0, 255, 0)
-COLOR_BLUE = (0, 0, 255)
-
-IMAGE_TILE = pygame.image.load("Tile.png")
+DISPLAY_SCRABBLE = pygame.display.set_mode((SIZE_DISPLAY_WIDTH,SIZE_DISPLAY_HEIGHT))
+pygame.display.set_caption("RLScrabble")
+DISPLAY_SCRABBLE.fill(ScrabbleBoard.Board.COLOR_WHITE)
 
 def RLScrabble():
     game_running = True
+    run_game()
     while game_running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.KEYUP:
                 game_running = False
         pygame.display.update()
 
+def run_game():
+    board = ScrabbleBoard.Board()
+    board.draw_board(DISPLAY_SCRABBLE)
+
 RLScrabble()
+pygame.quit()
+quit()
--- ScrabbleBoard.py
+++ ScrabbleBoard.py
@@ -11,69 +11,107 @@ Date edited:    2019/04/28
 '''
 
 import pygame
-pygame.init()
 
-SIZE_DISPLAY_WIDTH = 800
-SIZE_DISPLAY_HEIGHT = 600
-
-COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_RED = (255, 0, 0)
-COLOR_GREEN = (0, 255, 0)
-COLOR_BLUE = (0, 0, 255)
-
-IMAGE_TILE = pygame.image.load("Tile.png")
-
-ScrabbleDisplay = pygame.display.set_mode((SIZE_DISPLAY_WIDTH,SIZE_DISPLAY_HEIGHT))
-ScrabbleDisplay.fill(COLOR_WHITE)
-pygame.display.set_caption("RLScrabble")
-
-pixelArray = pygame.PixelArray(ScrabbleDisplay)
-
-game_running = True
-
-class ScrabbleBoard:
+class Board:
     DIMENSION_BOARD = 15
     POSITION_INITIAL = (7, 7)
 
-    SIZE_SQUARE_AREA = 32
-    SIZE_SQUARE_BORDER = 4
+    PREMIUM_LETTER_2X = 'L2'
+    PREMIUM_LETTER_3X = 'L3'
+    PREMIUM_WORD_2X = 'W2'
+    PREMIUM_WORD_3X = 'W3'
+
+    POSITIONS_LETTER_2X = [(0, 3), (0, 11),
+                           (2, 6), (2, 8),
+                           (3, 0), (3, 7), (3, 14),
+                           (6, 2), (6, 6), (6, 8), (6, 12),
+                           (7, 3), (7, 11),
+                           (8, 2), (8, 6), (8, 8), (8, 12),
+                           (11, 0), (11, 7), (11, 14),
+                           (12, 6), (12, 8),
+                           (14, 3), (14, 11)]
+
+    POSITIONS_LETTER_3X = [(1, 5), (1, 9),
+                           (5, 1), (5, 5), (5, 9), (5, 13),
+                           (9, 1), (9, 5), (9, 9), (9, 13),
+                           (13, 5), (13, 9)]
+
+    POSITIONS_WORD_2X = [(1, 1), (1, 13),
+                         (2, 2), (2, 12),
+                         (3, 3), (3, 11),
+                         (4, 4), (4, 10),
+                         (7, 7),
+                         (10, 4), (10, 10),
+                         (11, 3), (11, 11),
+                         (12, 2), (12, 12),
+                         (13, 1), (13, 13)]
+
+    POSITIONS_WORD_3X = [(0, 0), (0, 7), (0, 14),
+                         (7, 0), (7, 14),
+                         (14, 0), (14, 7), (14, 14)]
+
+    COLOR_BLACK = (0, 0, 0)
+    COLOR_WHITE = (255, 255, 255)
+    COLOR_RED = (255, 0, 0)
+    COLOR_GREEN = (0, 255, 0)
+    COLOR_BLUE = (0, 0, 255)
+
+    COLOR_TILE_BORDER = (100, 100, 100)
+
+    COLOR_DEFAULT = (30, 150, 0)
+    COLOR_LETTER_2X = (100, 200, 255)
+    COLOR_LETTER_3X = (0, 100, 255)
+    COLOR_WORD_2X = (255, 100, 0)
+    COLOR_WORD_3X = (255, 0, 0)
+
+    SIZE_SQUARE_AREA = 48
+    SIZE_SQUARE_BORDER = 0
     SIZE_SQUARE_GAP = 4
 
+    DISTANCE_TOP = 80
+    DISTANCE_LEFT = 60
+
     def __init__(self):
-        '''
-        Create an empty 2D array as the basis of the board:
-        '''
         self.square = []
-        for x in range(self.DIMENSION_BOARD):
+        for x in range(Board.DIMENSION_BOARD):
             self.square.append([])
-            for y in range(self.DIMENSION_BOARD):
+            for y in range(Board.DIMENSION_BOARD):
                 self.square[x].append((None, None))
 
-    def draw_board(self):
-        for row in range(self.DIMENSION_BOARD):
-            for column in range(self.DIMENSION_BOARD):
-                x = row * (self.SIZE_SQUARE_AREA + self.SIZE_SQUARE_BORDER) + self.SIZE_SQUARE_GAP
-                y = column * (self.SIZE_SQUARE_AREA + self.SIZE_SQUARE_BORDER) + self.SIZE_SQUARE_GAP
+        for (x, y) in self.POSITIONS_LETTER_2X:
+            self.square[x][y] = (None, Board.PREMIUM_LETTER_2X)
 
-                pygame.draw.rect(ScrabbleDisplay, COLOR_BLUE, (x_position, y_position, (self.SIZE_SQUARE_AREA + self.SIZE_SQUARE_BORDER), (self.SIZE_SQUARE_AREA + self.SIZE_SQUARE_BORDER)))
+        for (x, y) in self.POSITIONS_LETTER_3X:
+            self.square[x][y] = (None, Board.PREMIUM_LETTER_3X)
 
-                # ScrabbleDisplay.blit(IMAGE_TILE, (x_position,y_position))
+        for (x, y) in self.POSITIONS_WORD_2X:
+            self.square[x][y] = (None, Board.PREMIUM_WORD_2X)
 
-    def game_loop():
-        while game_running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    game_running = False
-                # print(event)
-            board = ScrabbleBoard()
-            ScrabbleDisplay.fill(COLOR_WHITE)
-            board.draw_board()
+        for (x, y) in self.POSITIONS_WORD_3X:
+            self.square[x][y] = (None, Board.PREMIUM_WORD_3X)
 
-            pygame.display.update()
+    def draw_board(self, DISPLAY_SCRABBLE):
+        for x in range(Board.DIMENSION_BOARD):
+            for y in range(Board.DIMENSION_BOARD):
+                x_position = Board.DISTANCE_TOP + x * (Board.SIZE_SQUARE_AREA + Board.SIZE_SQUARE_BORDER + Board.SIZE_SQUARE_GAP)
+                y_position = Board.DISTANCE_LEFT + y * (Board.SIZE_SQUARE_AREA + Board.SIZE_SQUARE_BORDER + Board.SIZE_SQUARE_GAP)
 
-game = ScrabbleBoard()
-game.game_loop()
+                # pygame.draw.rect(DISPLAY_SCRABBLE, COLOR_BLUE2,
+                #                 (x,y,(Board.SIZE_SQUARE_AREA + Board.SIZE_SQUARE_BORDER),(Board.SIZE_SQUARE_AREA + Board.SIZE_SQUARE_BORDER)))
 
-pygame.quit()
-quit()
+                if self.square[x][y][1] == Board.PREMIUM_LETTER_2X:
+                    square_color = Board.COLOR_LETTER_2X
+                elif self.square[x][y][1] == Board.PREMIUM_LETTER_3X:
+                    square_color = Board.COLOR_LETTER_3X
+                elif self.square[x][y][1] == Board.PREMIUM_WORD_2X:
+                    square_color = Board.COLOR_WORD_2X
+                elif self.square[x][y][1] == Board.PREMIUM_WORD_3X:
+                    square_color = Board.COLOR_WORD_3X
+                else:
+                    square_color = Board.COLOR_DEFAULT
+
+                pygame.draw.rect(DISPLAY_SCRABBLE, Board.COLOR_TILE_BORDER,
+                                 (x_position + 2, y_position + 2, Board.SIZE_SQUARE_AREA - 3, Board.SIZE_SQUARE_AREA - 3))
+
+                pygame.draw.rect(DISPLAY_SCRABBLE, square_color,
+                                 (x_position + 3, y_position + 3, Board.SIZE_SQUARE_AREA - 5, Board.SIZE_SQUARE_AREA - 5))
