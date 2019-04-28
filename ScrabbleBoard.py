e3b5d2acb3e091083fd39eb93fc30490a50c34f5
Unknown <matiss.apinis@gmail.com>
2019-04-28 16:07:06 +0300

    Added game board class

---
+++ ScrabbleBoard.py
@@ -0,0 +1,13 @@
+'''
+University of Latvia Faculty of Computing Qualification paper:
+Implementation of the board game "Scrabble"
+
+ScrabbleBoard.py:
+Contains the class modelling the basic "Scrabble" game board.
+
+Author:         Matīss Apinis (ma17058)
+Date created:   2019/04/28
+Date edited:    2019/04/28
+'''
+
+print("Hello, RLScrabble!")
