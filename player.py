638ada112595f934201e86c5dc262c192db5ae17
Unknown <matiss.apinis@gmail.com>
2019-05-21 20:25:26 +0300

    Added tile taker function to player-user class

--- player.py
+++ player.py
@@ -28,6 +28,25 @@ class Player:
         self.total_score = 0
         self.delta_score = 0
 
+    def take_tiles(self):
+        if not self.bag.is_bag_empty():
+            count_missing_tiles = rack.Rack.DIMENSION_RACK - len(self.rack)
+
+            for i in range(count_missing_tiles):
+                pulled_tile = self.bag.pull_tile()
+
+                if pulled_tile != None:
+                    self.rack.append(pulled_tile)
+
+        # If bag is empty and player rack is empty then the returned 'False' indicates the game has ended.
+        elif len(self.tray) == 0:
+            return False
+
+        return True
+
+    # def play_move(self):
+
+
 
 
 
